9bd101860f7e17734a33278f429ac336bbc01a5d
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2025-07-06 22:46:40 +0300

    Use C locale for the date command.

--- newvers.sh
+++ newvers.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+#
+# Use C locale for the date command, regardless of the user's locale.
+# This way, the date will be in English and not in the user's language,
+# if it doesn't happen to be English.
+#
+LC_TIME=C
+
 t=$(date +'%Y/%m/%d %H:%M:%S')
 d=$(date +'%Y/%m/%d')
+ti=$(date)
 
 rmj=1
 rmi=$(git rev-list HEAD --count)
@@ -12,5 +20,5 @@ cat > vers.c << EOF
 
 const CHAR16 version[] = L"Version 1.0";
 const CHAR16 revision[] = L"Internal revision ${rmj}.${rmi}, ${d}";
-const CHAR16 blddate[] = L"${t}";
+const CHAR16 blddate[] = L"${ti}";
 EOF
