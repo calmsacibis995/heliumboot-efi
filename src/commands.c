aa0465c7b8441e342a31058f51d3fe4db2766d0d
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2025-07-28 22:12:18 +0300

    Add license disclaimers to every source file.

--- src/commands.c
+++ src/commands.c
@@ -1,6 +1,21 @@
 /*
+ * HeliumBoot/EFI - A simple UEFI bootloader.
+ *
  * Copyright (c) 2025 Stefanos Stefanidis.
  * All rights reserved.
+ *
+ * This file is part of HeliumBoot/EFI.
+ *
+ * HeliumBoot/EFI is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * HeliumBoot/EFI is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with HeliumBoot/EFI. If not, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #include <efi.h>
