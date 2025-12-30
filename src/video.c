18501e731dd354405ad043008b637786f47d5b83
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2025-12-30 23:50:11 +0200

    Add some more copyright notices.

--- src/video.c
+++ src/video.c
@@ -23,9 +23,39 @@
  * which can be found here: https://github.com/cdaffara/symbiandump-os1/tree/master/sf/os/kernelhwsrv/brdbootldr/ubootldr
  *
  * These portions of code are licensed under the Eclipse Public License v1.0.
+ * It is available at the URL "http://www.eclipse.org/legal/epl-v10.html".
  *
  * Copyright (c) 1996-2009 Nokia Corporation and/or its subsidiary(-ies).
  * All rights reserved.
+ *
+ * In addition, some code is derived from GNU-EFI, adapted for this
+ * graphics renderer. Specifically, the functions "InputToScreen()" and
+ * "InputToScreen_Internal()".
+ *
+ * Copyright (c) 1998-2000 Intel Corporation
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and
+ * the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE. THE EFI SPECIFICATION AND ALL OTHER INFORMATION
+ * ON THIS WEB SITE ARE PROVIDED "AS IS" WITH NO WARRANTIES, AND ARE SUBJECT
+ * TO CHANGE WITHOUT NOTICE.
  */
 
 #include <efi.h>
