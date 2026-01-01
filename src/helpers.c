3c2823461c6bedaa4186a600fff949e5fef92ebd
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2026-01-01 12:13:20 +0200

    We are now in 2026.

--- src/helpers.c
+++ src/helpers.c
@@ -1,7 +1,7 @@
 /*
  * HeliumBoot/EFI - A simple UEFI bootloader.
  *
- * Copyright (c) 2025 Stefanos Stefanidis.
+ * Copyright (c) 2025, 2026 Stefanos Stefanidis.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
